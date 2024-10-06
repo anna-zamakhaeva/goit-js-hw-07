6346234e1e85f70d089141e609b12e1e60cffee9
Anna Zamakhaeva <zamakhaeva@gmail.com>
2024-10-06 16:32:11 +0300

    add task five

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector("span.color");
+
+colorBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.innerHTML = `${getRandomHexColor()}`;
+}
