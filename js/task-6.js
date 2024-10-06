aa0577369ff20490109011ca99c411dd7b142b40
Anna Zamakhaeva <zamakhaeva@gmail.com>
2024-10-06 16:02:28 +0300

    add task one and six

---
+++ js/task-6.js
@@ -0,0 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+let sizes = 30;
+
+createBtn.addEventListener("click", createMarkup);
+destroyBtn.addEventListener("click", clear);
+
+function clear() {
+  boxes.innerHTML = "";
+  input.value = "";
+  sizes = 30;
+}
+
+function createMarkup() {
+  if (input.value < 1 || input.value > 100) {
+    return;
+  }
+
+  createBoxes(input.value);
+}
+
+function createBoxes(amount) {
+  clear();
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${sizes}px`;
+    box.style.height = `${sizes}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxes.append(box);
+    sizes += 10;
+  }
+}
